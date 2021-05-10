072eeaeca9f9edaeae930d6b0575732aace950e6
Dmitrii Martynov <dmitriimartynov1@gmail.com>
2021-05-10 18:42:07 +0300

    finished fetching data section

--- src/utils/api-client.js
+++ src/utils/api-client.js
@@ -1,6 +1,19 @@
-export * from './api-client.final'
+function client(endpoint, customConfig = {}) {
+  const config = {
+    method: 'GET',
+    ...customConfig,
+  }
 
-// export * from './api-client.exercise'
+  return window
+    .fetch(`${process.env.REACT_APP_API_URL}/${endpoint}`, config)
+    .then(async response => {
+      const data = await response.json()
+      if (response.ok) {
+        return data
+      } else {
+        return Promise.reject(data)
+      }
+    })
+}
 
-// 💯 handle failed requests
-// export * from './api-client.extra-1'
+export {client}
