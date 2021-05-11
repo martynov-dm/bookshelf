12b97e8568c710f1c026047255b26143224eb17a
Dmitrii Martynov <dmitriimartynov1@gmail.com>
2021-05-11 12:24:29 +0300

    finished auth section

--- src/app.exercise.js
+++ src/app.exercise.js
@@ -2,24 +2,77 @@
 import {jsx} from '@emotion/core'
 
 import * as React from 'react'
-// 🐨 you're going to need this:
-// import * as auth from 'auth-provider'
+import * as auth from 'auth-provider'
 import {AuthenticatedApp} from './authenticated-app'
 import {UnauthenticatedApp} from './unauthenticated-app'
+import {client} from './utils/api-client'
+import {useAsync} from './utils/hooks'
+import {FullPageSpinner} from './components/lib'
+import * as colors from './styles/colors'
+
+async function getUser() {
+  let user = null
+
+  const token = await auth.getToken()
+  if (token) {
+    const data = await client('me', {token})
+    user = data.user
+  }
+  return user
+}
 
 function App() {
-  // 🐨 useState for the user
+  const {
+    data: user,
+    error,
+    isLoading,
+    isIdle,
+    isError,
+    isSuccess,
+    run,
+    setData,
+  } = useAsync()
 
-  // 🐨 create a login function that calls auth.login then sets the user
-  // 💰 const login = form => auth.login(form).then(u => setUser(u))
-  // 🐨 create a registration function that does the same as login except for register
+  React.useEffect(() => {
+    run(getUser())
+  }, [run])
 
-  // 🐨 create a logout function that calls auth.logout() and sets the user to null
+  const login = form => auth.login(form).then(user => setData(user))
+  const register = form => auth.register(form).then(user => setData(user))
+  const logout = () => {
+    auth.logout()
+    setData(null)
+  }
 
-  // 🐨 if there's a user, then render the AuthenticatedApp with the user and logout
-  // 🐨 if there's not a user, then render the UnauthenticatedApp with login and register
+  if (isLoading || isIdle) {
+    return <FullPageSpinner />
+  }
 
-  return <UnauthenticatedApp />
+  if (isError) {
+    return (
+      <div
+        css={{
+          color: colors.danger,
+          height: '100vh',
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}
+      >
+        <p>There's a problem. Try refreshing the page</p>
+        <pre>{error.message}</pre>
+      </div>
+    )
+  }
+
+  if (isSuccess) {
+    return user ? (
+      <AuthenticatedApp user={user} logout={logout} />
+    ) : (
+      <UnauthenticatedApp login={login} register={register} />
+    )
+  }
 }
 
 export {App}
